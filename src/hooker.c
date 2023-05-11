eb2dda7aa05d33e39a89bfdb0a843dd2457bf58d
EstebanFlorence <asap.bacco@protonmail.com>
2023-05-11 14:36:52 +0200

    zoom

--- src/hooker.c
+++ src/hooker.c
@@ -6,7 +6,7 @@
 /*   By: adi-nata <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/06 18:29:52 by adi-nata          #+#    #+#             */
-/*   Updated: 2023/05/10 23:08:18 by adi-nata         ###   ########.fr       */
+/*   Updated: 2023/05/11 14:16:14 by adi-nata         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@
 void	ft_mlxhooks(t_fractol *fractol)
 {
 	mlx_hook(fractol->win, 2, 1L<<0, key_hook, fractol);
-	mlx_hook(fractol->win, 17, 0, zoom_hook, fractol);
+//	mlx_hook(fractol->win, 17, 0, exit, 1);
 	mlx_mouse_hook(fractol->win, zoom_hook, fractol);
 	mlx_loop(fractol->mlx);
 
@@ -46,31 +46,29 @@ int	key_hook(int key, t_fractol *fractol)
 
 int	zoom_hook(int button, int x, int y, t_fractol *fractol)
 {
-	double	old_x;
+/* 	double	old_x;
 	double	old_y;
 	double	new_x;
 	double	new_y;
 
 	old_x = fractol->x_max - fractol->x_min;
-	old_y = fractol->y_max - fractol->y_min;
+	old_y = fractol->y_max - fractol->y_min; */
 
 	if (button == ROT_UP)
 	{
-		new_x = old_x / 1.1;
-		new_y = old_y / 1.1;
+		fractol->zoom *= 1.1;
 	}
 	else if (button == ROT_DOWN)
 	{
-		new_x = old_x * 1.1;
-		new_y = old_y * 1.1;
+		fractol->zoom /= 1.1;
 	}
-	else
-		return (0);
-	fractol->x_min = fractol->x_min + ((old_x - new_x) / 2) - (x / fractol->zoom);
+/* 	else
+		return (0); */
+/* 	fractol->x_min = fractol->x_min + ((old_x - new_x) / 2) - (x / fractol->zoom);
 	fractol->x_max = fractol->x_min + new_x;
 	fractol->y_min = fractol->y_min + ((old_y - new_y) / 2) - (y / fractol->zoom);
 	fractol->y_max = fractol->y_min + new_y;
-
+ */
 	printf("button : %d\n", button);
 	printf("x : %d\n", x);
 	printf("y : %d\n", y);
